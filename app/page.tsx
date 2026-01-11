9793a4e39aa071045a29c085d255ef600d1ba2d5
speaktheo <speaktheo@protonmail.com>
2026-01-11 04:47:45 -0800

    Remove newsletter section from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { WhyMoringa } from '@/components/why-moringa';
 import { MoringaHistory } from '@/components/moringa-history';
 import { MoringaNutrition } from '@/components/moringa-nutrition';
 import { MoringaMalnutrition } from '@/components/moringa-malnutrition';
-import { Newsletter } from '@/components/newsletter';
 import { Footer } from '@/components/footer';
 
 export default function Home() {
@@ -15,7 +14,6 @@ export default function Home() {
       <MoringaNutrition />
       <MoringaMalnutrition />
       {/* Testimonials temporarily removed from homepage */}
-      <Newsletter />
       <Footer />
     </main>
   );
