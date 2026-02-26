cbc6f9777aa12376c3f9dcda2103c83e017012e8
speaktheo <speaktheo@protonmail.com>
2026-02-26 10:03:58 +0200

    Done! I've centered the food list by adding mx-auto and max-w-6xl to the list container. The list will now be centered on the page instead of justifying to the left. ✓

--- app/(main)/dashboard/library/foods/views/explore-view.tsx
+++ app/(main)/dashboard/library/foods/views/explore-view.tsx
@@ -608,7 +608,7 @@ export function ExploreView({
             )}
 
             {/* List Area */}
-            <div className="space-y-0">
+            <div className="space-y-0 mx-auto max-w-6xl">
                 {groupNames.map((groupName) => {
                     const items = groupedFoods[groupName];
                     const isExpanded = expandedGroups[groupName] || (searchQuery.length > 0 && items.length > 0);
