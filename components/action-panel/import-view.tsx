3d47cbd34c7d1780f64fd82dfe79986418075f13
speaktheo <speaktheo@protonmail.com>
2026-04-19 09:27:51 +0000

    Make dialogue card full width with 1px padding

--- components/action-panel/import-view.tsx
+++ components/action-panel/import-view.tsx
@@ -273,7 +273,7 @@ export function ImportView({
 
             {/* Maker Slide-out */}
             <div className={cn(
-                'overflow-hidden transition-all duration-300 mx-4',
+                'overflow-hidden transition-all duration-300 px-[1px]',
                 showMakerInline ? 'max-h-[2000px] opacity-100' : 'max-h-0 opacity-0'
             )}>
                 <div className="mt-4">
